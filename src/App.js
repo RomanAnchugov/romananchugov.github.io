4c186b1bf905cd8960238d6e3e14bf88e9dcc33b
romanancugov <romagn@mail.ru>
2025-04-17 22:08:57 +0300

    oh fuck(

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 function App() {
   return (
